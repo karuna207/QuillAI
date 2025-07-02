c4628c49ec2a9a556d433d44b5232d6dab50b0b0
karunanidhi <karunanidhiayyamperumal@gmail.com>
2025-07-02 08:37:15 +0530

    Login API created

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
 import connectDB from './configs/db.js';
+import adminRouter from './routes/adminRoutes.js';
 
 const app=express(); 
 
@@ -11,7 +12,8 @@ await connectDB();
 //Middlewares
     
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); 
+app.use('/api/admin',adminRouter);
 
 app.get('/',(req,res)=>{
     res.send("API is working");
