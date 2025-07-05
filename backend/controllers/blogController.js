7d9c6ed8999fafbd955c6fce8bd1cc77dc78f5c8
karunanidhi <karunanidhiayyamperumal@gmail.com>
2025-07-05 21:19:49 +0530

    Blog API created

---
+++ backend/controllers/blogController.js
@@ -0,0 +1,127 @@
+import fs from 'fs';
+import imagekit from '../configs/imageKit.js';
+import Blog from '../models/Blog.js';
+
+export const addBlog = async (req, res) => {
+    try {
+        const { title, subTitle, description, category, isPublished } = JSON.parse(req.body.blog);
+        const imageFile = req.file;
+        if (!title || !description || !category || !imageFile) {
+            return res.json({
+                success: false,
+                message: "Missing required fields"
+            })
+        }
+
+        const fileBuffer = fs.readFileSync(imageFile.path);
+
+        // Upload Image to imagekit
+        const response = await imagekit.upload({
+            file: fileBuffer,
+            fileName: imageFile.originalname,
+            folder: "/blogs",
+        })
+
+        //Optimise the image 
+        const optimisedImageURL = imagekit.url(
+            {
+                path: response.filePath,
+                transformation: [
+                    { quality: 'auto' },  //Auto compression
+                    { format: 'webp' },   //modern format
+                    { width: '1280' }      //width resizing
+                ]
+            }
+        );
+
+        const image = optimisedImageURL;
+
+        await Blog.create({
+            title, subTitle, description, category, image, isPublished
+        });
+
+        res.json({
+            success: true,
+            message: "Blog added successfully"
+        })
+    } catch (error) {
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+export const getAllBlogs = async (req, res) => {
+    try {
+        const blogs = await Blog.find(
+            {
+                isPublished: true
+            }
+        );
+        res.json({
+            success: true,
+            blogs
+        })
+    } catch (error) {
+        console.log(error.message());
+        res.json({
+            success: false,
+            message: error.message
+        });
+    }
+}
+
+
+export const getBlogById = async (req, res) => {
+    try {
+        const { blogId } = req.params;
+        const blog = await Blog.findById(blogId);
+        if (!blog) {
+            return res.json({ success: false, message: "Blog not found" });
+        }
+        res.json({
+            success: true, blog
+        })
+    } catch (error) {
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+export const deleteBlogById = async (req, res) => {
+    try {
+        const { id } = req.body;
+        const blog = await Blog.findByIdAndDelete(id);
+        res.json({
+            success:true,
+            blog
+        });
+
+    } catch (error) {
+        res.json({
+            success:false,
+            message:"Some error occured"
+        })
+    }
+} 
+
+export const togglePublish=async (req,res)=>{
+    try {
+        const { id } = req.body; 
+        const blog=await Blog.findById(id);
+        blog.isPublished=!blog.isPublished;
+        await blog.save();
+        res.json({
+            success:true,
+            message:"Blog status updated"
+        })
+    } catch (error) {
+        res.json({
+            success:true,
+            message:"Error occured"
+        })
+    }   
+}
